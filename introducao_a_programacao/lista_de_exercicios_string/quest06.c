5c17c63b430f40e0e6dcb8a23925f60194f7ad33
Rafakkj0 <134242702+Rafakkj0@users.noreply.github.com>
2026-05-18 07:52:47 -0300

    Update questão 06

--- introducao_a_programacao/lista_de_exercicios_string/quest06.c
+++ introducao_a_programacao/lista_de_exercicios_string/quest06.c
@@ -1,21 +1,36 @@
-// Crie uma função que recebe uma string e um caractere, e apague todas as ocorrências desses
-// caractere na string.
+// Crie uma função que recebe uma string e um caractere, e apague todas as ocorrências desses caractere na string
 
 #include <stdio.h>
 #include "mylib.h"
-#define TAMANHO 101
 
+#define MAX_CARACTER 100
 
+void removeCaractere(char str[], char c){
+    int i, k;
+    for(i=0; str[i] ;i++){
+        if(str[i] == c){
+            for(k=i; str[k]; k++){
+                str[k] = str[k+1];
+            }
+            i--;
+        }
+    }
+}
 
 int main(){
-    int txt[TAMANHO], c;
-    printf("\n- Forneça o texto com no máximo %d caracteres:\n", TAMANHO -1);
-    lerString(txt, TAMANHO);
+    char txt[MAX_CARACTER], c;
 
-    printf("- Forneça o caractere que deseja remover: ");
-    scanf("%c", c);
+    printf("\n----- Removedor de caracteres -----\n");
+    printf("Escreva o texto que deseja remover um caractere:\n\n");
+    lerString(txt, MAX_CARACTER);
 
+    printf("\nQual caractere deseja remover: ");
+    scanf("%c", &c);
 
+    removeCaractere(txt, c);
+
+    printf("Texto com os caracteres '%c' removidos:\n%s\n\n", c, txt);
+    
 
     return 0;
 }
