19c620932a27a8ed2d0eaab627359f9efc033261
Rafakkj0 <rafaelhenrique200884@gmail.com>
2026-06-23 10:39:58 -0300

    Feito um pouco mais - questão de struct sbre funcionarios

--- introducao_a_programacao/aula-2026-06-19-struct/funcionario.c
+++ introducao_a_programacao/aula-2026-06-19-struct/funcionario.c
@@ -1,49 +1,110 @@
 #include <stdio.h>
 #include <string.h>
 #include "funcionario.h"
+void limpaBuffer(){
+    int c;
+    do{
+        c = getchar();
+    }while(c!='\n');
+}
 
-typedef struct funcionarios{
-    int matricula;
-    char nome[100];
-    float salario;
-}TFuncionario;
+int pesquisa(TFuncionario v[], int ultimaPos, int mat){
+    int i=0;
+    int achou=0;
+    while(i<=ultimaPos && achou==0){
+        if(v[i].matricula==mat)
+            achou=1;
+        else
+            i++;
+    }
+    if(achou == 1) return i;
+    else return -1;
+}
 
-void cadastrar(TFuncionario v[], int tamanho, int numFuncionarios){
-    if(numFuncionarios==tamanho){
-        printf("\nNúmero máximo de funcionarios aingida!\n");
+int cadastrar(TFuncionario v[],int tamanho,int *ultimaPos){
+    int mat,achou,i,t;
+    if(*ultimaPos==tamanho-1){
+        printf("\nVetor Cheio\n");
+        return 0;
     }
     else{
-        printf("\nForneça a matricula: ");
-        int matricula;
-        scanf("%d", &matricula);
-
-        int achou = 0;
-        for(int i = 0; i < numFuncionarios && achou == 0; i++){
-            if(matricula == v[i].matricula) {
-                achou = 1;
-            }
-        }
-        if(achou == 1){
-            printf("\nMatricula já cadastrada!\n");
-            return;
+        printf("Forneça a matrícula:");
+        scanf("%d",&mat);
+        if(pesquisa(v, *ultimaPos, mat) != -1){
+            printf("\nMatrícula Repetida!!!\n");
+            return 0;
         }
         else{
-            v[numFuncionarios].matricula = matricula;
-            printf("Fornaça o nome: ");
+            (*ultimaPos)++;
+            v[*ultimaPos].matricula=mat;
+            printf("Forneça o nome:");
             limpaBuffer();
-            fgets(v[numFuncionarios].nome, 100, stdin);
-            // Verificando se tem algo no buffer de entrada.
-            int tam = strlen(v[numFuncionarios].nome);
-            if(v[numFuncionarios].nome[tam-1]=='\n'){
-                v[numFuncionarios].nome[tam-1]='\0';
-            }
-            else limpaBuffer();
-
-            printf("Forneça o salario: ");
-            scanf("%f", &v[numFuncionarios].salario);
-
-            printf("\nFuncionario cadastrado com suceso.\n");
-            numFuncionarios++;
+            fgets(v[*ultimaPos].nome,100,stdin);
+            //verificando se tem algo no buffer de entrada
+            t = strlen(v[*ultimaPos].nome);
+            if(v[*ultimaPos].nome[t-1]=='\n')
+                v[*ultimaPos].nome[t-1]='\0';
+            else
+                limpaBuffer();
+            printf("Forneça o salário:");
+            scanf("%f",&v[*ultimaPos].salario);
+            return 1;
         }
     }
+}
+
+void consultar(TFuncionario v[], int ultimaPos){
+    int mat;
+    //verificando se tem pelo menos um funcionário cadastrado
+    if(ultimaPos>=0){
+        printf("\nForneça a matricula: ");
+        scanf("%d", &mat);
+        int posFuncionario = pesquisa(v, ultimaPos, mat);
+        if(posFuncionario != -1){
+            printf("\nDados do funconario:\n");
+            printf("Matrícula: %d\n",v[posFuncionario].matricula);
+            printf("Nome: %s\n",v[posFuncionario].nome);
+            printf("Salário: %.1f\n",v[posFuncionario].salario);
+        }
+        else{
+            printf("\nFuncionario não encontrado!\n");
+        }
+    }
+    else
+        printf("\nNenhum Funcionário Cadastrado!!!\n");
+}
+
+int excluir(TFuncionario v[],int *ultimaPos){
+    int mat;
+    if(*ultimaPos>=0){
+        printf("\nForneça a matricula: ");
+        scanf("%d", &mat);
+        int posFuncionario = pesquisa(v, *ultimaPos, mat);
+        if(posFuncionario != -1){
+            if(posFuncionario != *ultimaPos) v[posFuncionario] = v[*ultimaPos];
+            (*ultimaPos)--;
+            return 0;
+        }
+        else{
+            printf("\nFuncionario não encontrado!\n");
+        }
+    }
+    else
+        printf("\nNenhum Funcionário Cadastrado!!!\n");
+    return 1;
+}
+
+void listar(TFuncionario v[],int ultimaPos){
+    int i;
+    //verificando se tem pelo menos um funcionário cadastrado
+    if(ultimaPos>=0){
+        printf("\nFuncionários Cadastrados\n");
+        for(i=0;i<=ultimaPos;i++){
+            printf("Matrícula: %d\n",v[i].matricula);
+            printf("Nome: %s\n",v[i].nome);
+            printf("Salário: %.1f\n",v[i].salario);
+        }
+    }
+    else
+        printf("\nNenhum Funcionário Cadastrado!!!\n");
 }
--- introducao_a_programacao/aula-2026-06-19-struct/main.c
+++ introducao_a_programacao/aula-2026-06-19-struct/main.c
@@ -5,108 +5,52 @@
 4 - listar
 5 – Fim
 caso a opção selecionada seja:
-1 – Permitir que se cadastre a matrícula, nome e salário de no máximo 30 funcionários.
+1 – Permitir que se cadastre a matrícula, nome e salário de no 
+máximo 30 funcionários.
 Obs.:
 a) A matrícula é única
 b) Aceitar apenas valores positivos para o campo salário
-2 – Pedir ao usuário uma valor e mostrar os dados do funcionário que possua matrícula igual ao valor
-fornecido.
-3 – Pedir ao usuário uma valor e excluir o funcionário que possua matrícula igual ao valor fornecido.
+
+2 – Pedir ao usuário uma valor e mostrar os dados do funcionário que 
+possua matrícula igual ao valor fornecido.
+
+3 – Pedir ao usuário uma valor e excluir o funcionário que possua 
+matrícula igual ao valor fornecido.
+
 4 – Listar os dados de todos os funcionários
 */
-
 #include <stdio.h>
 #include <string.h>
-#define MAX_FUNCIONARIOS 30
-
-void limpaBuffer(){
-    while(getchar() != '\n');
-}
-
-
-typedef struct funcionarios{
-    int matricula;
-    char nome[100];
-    float salario;
-}TFuncionario;
-
+#include "funcionario.h"
+#define NUMERO_MAXIMO_FUNCIONARIOS 1000
 int main(){
-    TFuncionario vf[MAX_FUNCIONARIOS];
-    int numFuncionarios = 0;
-
-    while(1){
-        printf("\n1- Cadastrar\n2- Consultar\n3- Excluir\n4- Listar\n5- Fim\n>");
-        int entrada;
-        scanf("%d", &entrada);
-
-        switch (entrada)
-        {
-        case 1: // Cadastrar funcionario
-            if(numFuncionarios==MAX_FUNCIONARIOS){
-                printf("\nNúmero máximo de funcionarios aingida!\n");
-            }
-            else{
-                printf("\nForneça a matricula: ");
-                int matricula;
-                scanf("%d", &matricula);
-
-                int achou = 0;
-                for(int i = 0; i < numFuncionarios && achou == 0; i++){
-                    if(matricula == vf[i].matricula) {
-                        achou = 1;
-                    }
+    TFuncionario vf[NUMERO_MAXIMO_FUNCIONARIOS];
+    int ultimaPosicao=-1,opcao;
+    do{
+        printf("\n\n1-Cadastrar\n2-Consultar\n3-Excluir\n4-Listar\n5-Fim");
+        printf("\nFoneça sua opção:");
+        scanf("%d",&opcao);
+        switch(opcao){
+            case 1:
+                if(cadastrar(vf,NUMERO_MAXIMO_FUNCIONARIOS,&ultimaPosicao)==1)
+                    printf("Funcionário Cadastrado co Sucesso!!!\n");
+                else    
+                    printf("\nNão foi possível realizar o cadastro!!!\n");
+                break;
+            case 2:
+                consultar(vf, ultimaPosicao);
+                break;
+            case 3:
+                if(excluir(vf, &ultimaPosicao) == 0){
+                    printf("\nFuncionario excluido com sucesso.\n");
                 }
-                if(achou == 1){
-                    printf("\nMatricula já cadastrada!\n");
-                    break;
-                }
-                else{
-                    vf[numFuncionarios].matricula = matricula;
-                    printf("Fornaça o nome: ");
-                    limpaBuffer();
-                    fgets(vf[numFuncionarios].nome, 100, stdin);
-                    // Verificando se tem algo no buffer de entrada.
-                    int tam = strlen(vf[numFuncionarios].nome);
-                    if(vf[numFuncionarios].nome[tam-1]=='\n'){
-                        vf[numFuncionarios].nome[tam-1]='\0';
-                    }
-                    else limpaBuffer();
-
-                    printf("Forneça o salario: ");
-                    scanf("%f", &vf[numFuncionarios].salario);
-
-                    printf("\nFuncionario cadastrado com suceso.\n");
-                    numFuncionarios++;
-                }
-            }
-            break;
-        case 2:
-            
-            break;
-        case 3:
-            
-            break;
-        case 4:
-            printf("\nFuncionarios cadastrados:\n");
-            if(numFuncionarios == 0){
-                printf("Nenhum funcionario cadastrado!\n");
-            }
-            else{
-                for(int i=0; i < numFuncionarios; i++){
-                    printf("Matricula: %d\n", vf[i].matricula);
-                    printf("Nome: %s\n", vf[i].nome);
-                    printf("Salário: R$%.2f\n", vf[i].salario);
-                    printf("=====================\n");
-                }
-            }
-            break;
-        case 5:
-            break;
-        default:
-            printf("\nValor invalido!\n");
-            break;
+                break;
+            case 4:
+                listar(vf,ultimaPosicao);
+                break;
         }
-        if(entrada == 5) break;
-    }
+    }while(opcao != 5);
     return 0;
 }
+
+
